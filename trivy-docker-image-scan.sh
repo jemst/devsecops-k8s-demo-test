4fea0e2aafb514fd2651f08ddc0f879d2e69c659
jemst <94155923+jemst@users.noreply.github.com>
2021-12-06 10:46:43 -0500

    changing openjdk back to original

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -9,7 +9,7 @@ docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit
 
   if [[ "${exit_code}" == 1 ]]; then
     echo "Image scanning failed. Vulnerabilities found"
-    #exit 1;
+    exit 1;
   else
     echo "Image scanning passed. No CRITICAL vulnerabilities found"
   fi;
